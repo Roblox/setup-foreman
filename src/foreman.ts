a71437f07c7d503bbcdccb16e1f43c37f77d786f
Lucien Greathouse <me@lpghatguy.com>
2020-02-21 13:45:17 -0800

    Initial commit

---
+++ src/foreman.ts
@@ -0,0 +1,70 @@
+import core from "@actions/core";
+import axios from "axios";
+import semver from "semver";
+
+const RELEASE_URL = "https://api.github.com/repos/rojo-rbx/foreman/releases";
+
+interface GitHubAsset {
+  name: string;
+  url: string;
+}
+
+interface GitHubRelease {
+  tag_name: string;
+  assets: Array<GitHubAsset>;
+}
+
+async function getReleases(): Promise<Array<GitHubRelease>> {
+  const response = await axios.get(RELEASE_URL);
+
+  if (response.status === 200) {
+    const releases = response.data as Array<GitHubRelease>;
+    releases.sort((a, b) => -semver.compare(a.tag_name, b.tag_name));
+
+    return releases;
+  } else {
+    throw new Error("Could not fetch Foreman releases");
+  }
+}
+
+function chooseRelease(versionReq: string, releases: Array<GitHubRelease>): GitHubRelease | null {
+  for (const release of releases) {
+    if (semver.satisfies(release.tag_name, versionReq)) {
+      return release;
+    }
+  }
+
+  return null;
+}
+
+function chooseAsset(release: GitHubRelease): GitHubAsset | null {
+  let platformName;
+  if (process.platform === "win32") {
+    platformName = "windows";
+  } else if (process.platform === "darwin") {
+    platformName = "macos";
+  } else if (process.platform === "linux") {
+    platformName = "linux";
+  } else {
+    throw new Error(`Unsupported platform "${ process.platform }"`);
+  }
+
+  for (const asset of release.assets) {
+    if (asset.name.includes(platformName)) {
+      return asset;
+    }
+  }
+
+  return null;
+}
+
+function addToPath() {
+  core.addPath("~/.foreman/bin");
+}
+
+export default {
+  getReleases,
+  chooseRelease,
+  chooseAsset,
+  addToPath,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import foreman from "./foreman";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const versionReq: string = core.getInput("version");
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const releases = await foreman.getReleases();
 
-    core.setOutput('time', new Date().toTimeString())
+    const release = foreman.chooseRelease(versionReq, releases);
+    if (release == null) {
+      throw new Error(`Could not find Foreman release for version ${ versionReq }`);
+    }
+
+    const asset = foreman.chooseAsset(release);
+    if (asset == null) {
+      throw new Error(`Could not find asset for version ${ release.tag_name } on platform ${ process.platform }`);
+    }
+
+    const zipPath = await tc.downloadTool(asset.url);
+    const extractedPath = await tc.extractZip(zipPath, ".foreman-install");
+    core.addPath(".foreman-install");
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
